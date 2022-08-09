ed46eb665bb964254e1ad536285f42f6b7f9963c
anselm22 <110898825+anselm22@users.noreply.github.com>
2022-08-09 17:41:16 +0800

    Add files via upload

---
+++ nlnhs/calendar/js/demo.js
@@ -0,0 +1,6 @@
+var events = [
+  {'Date': new Date(2022, 7, 7), 'Title': 'Graduation Day!'},
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
